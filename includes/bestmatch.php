199387f30ddee84f5dbc400e6befb1eb524603d9
aimee <aimee@156.59.107.52>
2017-09-07 12:16:50 +1200

    send request update

--- includes/bestmatch.php
+++ includes/bestmatch.php
@@ -9,7 +9,9 @@ if (!isset($_SESSION['u_id'])){
 	}
         
  $user_uid = $_SESSION['u_id'];        
- $action = $_GET('act');
+ $action = stripslashes(trim($_GET['act'])); 
+ 
+ //echo $action;
  
  $want = array('a001','a002');
  $extra= array('a005','a006');
@@ -72,15 +74,26 @@ if($action == 'getJSON'){
      
 }
 
-else if($action == ''){
-    $reciver_uid = $id;
-    $row = count($finallist, 1);
+else if($action == 'sendRequest'){
+    $reciver_uid = $_POST('name');
+    error_log($reciver_uid,3,'my-errors.log');
+  //  $reciver_uid = $id;
+    $count = 0;
+    foreach($finallist as $subarray){
+    $count += count($subarray);
+    }
+    for($i = 0;$i<$finallist;$i++){
+        if($finallist[$i][0] == $reciver_uid){
+            $offerlist = $finallist[$i][1];
+            $misslist = $finallist[$i][2];
+        }
+    }
     
     
     $mail = new sendemail();
     $subject = "New Swap Request";
-    $body = "Dear".$reciver_uid."：<br/>".$user_uid."send you a swap request <br/>offer:".$offerlist.
-            "<br/>miss: ".$misslist;
+    $body = "Dear".$reciver_uid."：<br/>".$user_uid."send you a swap request <br/>you need to offer:".$offerlist.
+            "<br/>he/she can offer you: ".$misslist;
             
     $mail->sendEmail($email,$subject,$body);
     
