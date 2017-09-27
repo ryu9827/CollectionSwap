f8e63d07bf2b4624ece3dbc80e87cb68f4947ecb
aimee <aimee@156.59.107.52>
2017-09-27 13:40:14 +1300

    Merge origin/master
    Conflicts:
    	includes/message_acceptswap.php

--- includes/message_acceptswap.php
+++ includes/message_acceptswap.php
@@ -2,6 +2,56 @@
 
 include_once 'match.php';
 include_once 'dbh.inc.php';
+include_once 'sendMatchRequest.php';
+global $conn;
+ob_start(); 
+session_start();
+if (!isset($_SESSION['u_id'])){
+     header("location:../login.php");
+
+	}
+        
+ $user_uid = $_SESSION['u_uid'];   
+ $msg_id = $_POST['message_id'];
+
+ function getaddress($uname){
+ $sql = "SELECT * FROM users WHERE user_uid = '$uname'";
+ $res = mysqli_query($conn, $sql); 
+ $row = mysqli_fetch_assoc($res);
+ $address = $row['address'];
+ return $address;
+ }
+
+ 
+ $sql = "SELECT * FROM messages WHERE id = '$msg_id'";
+ $res = mysqli_query($conn, $sql);
+ $row = mysqli_fetch_assoc($res);
+ $token = $row['token'];
+ $ruid = $row['swap_uid'];
+ 
+ $address = getaddress($user_uid);
+ $raddress = getaddress($ruid);
+ 
+ $sql = "UPDATE messages SET status = '4' WHERE token = '$token'";
+ mysqli_query($conn, $sql);
+ 
+ $email = getemail($ruid);
+ $email = getemail($user_uid);
+ 
+ $mail = new sendemail();
+ $subject = "Your swap request has been Accepted!";
+ $body = "Dear ".$ruid."：<br/> Congratulations!<br/>Your swap request has been successfully accepted!<br/>Collection Set Name: "
+         .$set_name."<br/>Offer: ".$missname."<br/>Demand: ".$offername."<br/>Applicant Username or Acceptant Username: ".$ruid.
+         "<br/>Offer: ".$remails."<br/>Shipping details:<br/>Delivery address:  ".$address."<br/>";
+ $mail->sendEmail($email,$subject,$body);
+ 
+ $subject = "Swaping Details";
+ $body = "Dear ".$user_uid."：<br/> Your already accept the swap request from user: ".$ruid."<br/>Collection Set Name: "
+         .$set_name."<br/>Offer: ".$offername."<br/>Demand: ".$missname."<br/>Applicant Username or Acceptant Username: ".$ruid.
+         "<br/>Offer: ".$remails."<br/>Shipping details:<br/>Delivery address:  ".$raddress."<br/>";
+ $mail->sendEmail($email,$subject,$body);
+
+ header('location:../setsManagement_messages.php');
 include_once 'sendemail.inc.php';
 //include_once 'sendMatchRequest.php';
 global $conn;
