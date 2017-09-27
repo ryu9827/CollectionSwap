bc8c810ef6f050bca2b2cf7748b15a8929b7957e
aimee <aimee@156.59.107.52>
2017-09-27 18:15:20 +1300

    update set name in message

--- includes/showmessage.php
+++ includes/showmessage.php
@@ -20,15 +20,16 @@ $num = mysqli_num_rows($res);
 
 
 if($num > 0){
-$row = mysqli_fetch_assoc($res);
-$set_id = $row['set_id'];
 
-$sql = "SELECT * FROM sets_exist WHERE set_id = '$set_id'";
-$res2 = mysqli_query($conn, $sql);
-$row2 = mysqli_fetch_assoc($res2);
-$set_name = $row2['set_name'];
 //echo mysqli_num_rows($res);
-while($row){
+while($row= mysqli_fetch_assoc($res)){
+    
+    $set_id = $row['set_id'];
+
+    $sql = "SELECT * FROM sets_exist WHERE set_id = '$set_id'";
+    $res2 = mysqli_query($conn, $sql);
+    $row2 = mysqli_fetch_assoc($res2);
+    $set_name = $row2['set_name'];
     
     $msg = array('swap_uid' => $row['swap_uid'],'email' => $row['swap_email'],'status' => $row['status'],'time' => $row['time'],
                 'set_id' => $row['set_id'],'set_name' => $set_name,'offer_id' => $row['offer_id'], 'get_id' =>$row['get_id'], 'msg_id' => $row['id']
