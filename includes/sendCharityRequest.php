d16e5147843a1b681b141611d64ae645b2b8c5fd
aimee <aimee@156.59.107.52>
2017-10-12 18:43:43 +1300

    merge charity part

--- includes/sendCharityRequest.php
+++ includes/sendCharityRequest.php
@@ -6,6 +6,8 @@ include_once 'sendemail.inc.php';
 
 ob_start(); 
 session_start();
+global $conn;  
+
 if (!isset($_SESSION['u_id'])){
      header("location:../login.php");
 
@@ -20,14 +22,22 @@ date_default_timezone_set('NZ');
 $reciver_uid = mysqli_real_escape_string($conn, $_POST['name']);
 $set_id = mysqli_real_escape_string($conn,$_POST['set_id']);
 $charitylist = mysqli_real_escape_string($conn,$_POST['cardid']);
-//$misslist =  mysqli_real_escape_string($conn,$_POST['miss']);
+
 $charityname = mysqli_real_escape_string($conn,$_POST['cardname']);
-//$missname = mysqli_real_escape_string($conn,$_POST['missname']);
+
+
+
+//$reciver_uid = 'Bruce';
+//$set_id = '1';
+//$charitylist = '1001,1002';
+////$misslist =  mysqli_real_escape_string($conn,$_POST['miss']);
+//$charityname = '1001,1002';
 
 
 $user_uid = $_SESSION['u_uid'];
 $uid = $_SESSION['u_id'];
-
+//$user_uid = 'amy';
+//$uid = '50';
     
    function getemail($user_uid){ 
        global $conn;  
@@ -60,17 +70,19 @@ $uid = $_SESSION['u_id'];
 
    
    $regtime = time(); 
-   $token = md5($user_uid.$reciver_uid.$reqtime); 
+   $token = md5($user_uid.$reciver_uid.$regtime); 
    
     
 
   //create new message for both user and reciver
    $time = date('d/m/Y H:i:s');
-   $sql = "INSERT INTO messages (user_uid,swap_uid,swap_email,status,time,set_id,get_id,token,get_name,regtime) VALUES ('$user_uid','$reciver_uid','$uemail','7','$time','$set_id','$charitylist','$token','$charityname','$regtime')";
-   mysqli_query($conn, $sql);
+   $sql1 = "INSERT INTO messages (user_uid,swap_uid,swap_email,status,time,set_id,get_id,token,get_name,regtime) VALUES 
+           ('$user_uid','$reciver_uid','$uemail','7','$time','$set_id','$charitylist','$token','$charityname','$regtime')";
+   mysqli_query($conn, $sql1);
+   echo mysqli_error($conn);
   
-   $sql = "INSERT INTO messages (user_uid,swap_uid,swap_email,status,time,set_id,offer_id,token,offer_name,regtime) VALUES ('$reciver_uid','$user_uid','$remail','8','$time','$set_id','$charitylist','$token','$charityname','$regtime')";
-   mysqli_query($conn, $sql);
+   $sql2 = "INSERT INTO messages (user_uid,swap_uid,swap_email,status,time,set_id,offer_id,token,offer_name,regtime) VALUES ('$reciver_uid','$user_uid','$remail','8','$time','$set_id','$charitylist','$token','$charityname','$regtime')";
+   mysqli_query($conn, $sql2);
    
    
    
